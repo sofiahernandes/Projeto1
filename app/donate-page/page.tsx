3f2590e279abc2f0d97e7a728acbed3fde8cfac3
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2025-09-01 11:00:32 -0300

    style: Updates donation page styles

--- app/donate-page/page.tsx
+++ app/donate-page/page.tsx
@@ -1,97 +1,177 @@
 "use client";
 
-import "/styles/donatePage.css";
+import "./donatePage.css";
 import React, { useState } from "react";
-import Link from "next/link";
-import { Form } from "react-hook-form";
+import { Calendar } from "@/components/ui/calendar";
+import { ChevronDownIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export default function Donations() {
-    const [menuOpen, setMenuOpen] = useState(false)
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
+  const [calendarOpen, setCalendarOpen] = React.useState(false);
+  const [date, setDate] = React.useState<Date | undefined>(undefined);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-      const formDonate = new FormData(e.currentTarget);
-      const Donate = {
-        name: formDonate.get("name"),
-        date: formDonate.get("date"),
-        value: formDonate.get("value-donate"),
-        goal: formDonate.get("goal"),
-        funds: formDonate.get("funds"),
-        files: formDonate.get("files"),
-      };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-      console.log("Dados do formulario", Donate);
-      alert("Arrecadação cadastrada! Verifique o console: ");
-
+    const formDonate = new FormData(e.currentTarget);
+    const Donate = {
+      name: formDonate.get("name"),
+      date: formDonate.get("date"),
+      value: formDonate.get("value-donate"),
+      goal: formDonate.get("goal"),
+      funds: formDonate.get("funds"),
+      files: formDonate.get("files"),
     };
 
+    console.log("Dados do formulario", Donate);
+    alert("Arrecadação cadastrada! Verifique o console: ");
+  };
+
   return (
-    <div> 
-    <Link href="/" className="underline text-blue-700 fixed p-4 cursor-pointer">
-        <span> Voltar </span>  
-    </Link>  
-
-    <div className={`page-container ${menuOpen ? "shifted" : ""}`}>
+    <div className="container w-full md:mt-20">
       <header>
-        <button className={`open-menu ${menuOpen ? "hidden" : ""}`} onClick={() => setMenuOpen(true)}> ☰ Menu </button>
-        
-        <h1 className="page-title">Registro de arrecadações - Arkana</h1>
+        <button
+          className={`open-menu ${menuOpen ? "menu-icon hidden" : "menu-icon"}`}
+          onClick={() => setMenuOpen(true)}
+        >
+          {" "}
+          ☰{" "}
+        </button>
       </header>
+      <div className="content">
+        <div className={`page-container ${menuOpen ? "shifted" : ""}`}>
+          {/*Menu lateral quando está no desktop/tablet*/}
+          <aside className={`side-menu ${menuOpen ? "open" : ""}`}>
+            <button className="close-menu" onClick={() => setMenuOpen(false)}>
+              {" "}
+              ✖{" "}
+            </button>
+            <nav>
+              <button className="new-donation">
+                {" "}
+                Cadastrar novas contribuições{" "}
+              </button>
+              <button className="old-donations">
+                {" "}
+                Histórico de contribuições{" "}
+              </button>
+            </nav>
+          </aside>
 
-      {/*Menu lateral quando está no desktop/tablet*/}
-      <aside className={`side-menu ${menuOpen ? "open" : ""}`}>
-        <button className="close-menu" onClick={() => setMenuOpen(false)}> ✖ </button>
-        <nav>
-            <button className="new-donation"> Cadastrar novas contribuições </button>
-            <button className="old-donations"> Histórico de contribuições </button>
-        </nav>
-      </aside>
+          {/*Menu rodapé quando está no mobile*/}
+          <nav className="mobile-nav">
+            <button className="new-donation">
+              {" "}
+              Cadastrar novas contribuições{" "}
+            </button>
+            <button className="old-donations">
+              {" "}
+              Histórico de contribuições{" "}
+            </button>
+          </nav>
 
-      {/*Menu rodapé quando está no mobile*/}
-      <nav className="mobile-nav"> 
-            <button className="new-donation"> Cadastrar novas contribuições </button>
-            <button className="old-donations"> Histórico de contribuições </button>
-      </nav>
+          <main>
+            <div className="card-area">
+              <h2>Contribuições financeiras</h2>
+              <form className="donation-details" onSubmit={handleSubmit}>
+                <div className="name">
+                  <label>Nome do doador/evento:</label>
+                  <input
+                    placeholder="Ex: Instituto ALMA"
+                    type="text"
+                    name="name"
+                    className="block w-full bg-[white] border border-[#b4b4b4] rounded-lg text-black placeholder-gray-400 px-3 py-1.5 text-base focus:outline-none"
+                  />
+                </div>
 
-      <main> 
-        <div className="card-area" >
-          <h2>Contribuições financeiras</h2>
-          <form className="donation-details" onSubmit={handleSubmit}>
-            <div className="name">
-              <label>Nome do doador/evento:</label>
-              <input placeholder="Ex: Instituto ALMA" type="text"  name="name"/>
+                <div className="date">
+                  <label>Data:</label>
+                  <div className="block w-full bg-[white] border border-[#b4b4b4] rounded-lg text-black placeholder-gray-400 px-3 py-1.5 text-base focus:outline-none">
+                    <Popover open={calendarOpen} onOpenChange={setCalendarOpen}>
+                      <PopoverTrigger asChild>
+                        <Button
+                          variant="outline"
+                          id="date"
+                          className="justify-between font-normal"
+                        >
+                          {date
+                            ? date.toLocaleDateString()
+                            : "Selecione a data"}
+                          <ChevronDownIcon />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent
+                        id="calendar"
+                        className="w-auto overflow-hidden border-[#ccc] p-0"
+                        align="start"
+                      >
+                        <Calendar
+                          mode="single"
+                          defaultMonth={date}
+                          selected={date}
+                          onSelect={(date) => {
+                            setDate(date);
+                            setCalendarOpen(false);
+                          }}
+                        />
+                      </PopoverContent>
+                    </Popover>
+                  </div>
+                </div>
+
+                <div className="value-donate">
+                  <label>Valor arrecadado:</label>
+                  <input
+                    className="block w-full bg-[white] border border-[#b4b4b4] rounded-lg text-black placeholder-gray-400 px-3 py-1.5 text-base focus:outline-none"
+                    placeholder="R$"
+                    type="number"
+                    name="value-donate"
+                  />
+                </div>
+
+                <div className="goal">
+                  <label>Meta esperada:</label>
+                  <input
+                    className="block w-full bg-[white] border border-[#b4b4b4] rounded-lg text-black placeholder-gray-400 px-3 py-1.5 text-base focus:outline-none"
+                    placeholder="R$"
+                    type="number"
+                    name="goal"
+                  />
+                </div>
+
+                <div className="funds">
+                  <label>Gastos com evento/organização:</label>
+                  <input
+                    className="block w-full bg-[white] border border-[#b4b4b4] rounded-lg text-black placeholder-gray-400 px-3 py-1.5 text-base focus:outline-none"
+                    placeholder="R$"
+                    type="number"
+                    name="funds"
+                  />
+                </div>
+
+                <div className="files">
+                  <label>Anexar comprovantes</label>
+                  <input
+                    className="block w-full bg-[white] border border-[#b4b4b4] rounded-lg text-black placeholder-gray-400 px-3 py-1.5 text-base focus:outline-none"
+                    type="file"
+                    name="files"
+                  />
+                </div>
+
+                <button className="mt-5" type="submit">
+                  Cadastrar arrecadação
+                </button>
+              </form>
             </div>
-
-            <div className="date">
-              <label>Data:</label>
-              <input type="date" name="date" />
-            </div>
-
-            <div className="value-donate">
-              <label>Valor arrecadado:</label>
-              <input placeholder="R$" type="number" name="value-donate" />
-            </div>
-
-            <div className="goal">
-              <label>Meta esperada:</label>
-              <input placeholder="R$" type="number" name="goal" />
-            </div>
-
-            <div className="funds">
-              <label>Gastos com evento/organização:</label>
-              <input placeholder="R$" type="number" name="funds" />
-            </div>
-
-            <div className="files">
-              <label>Anexar comprovantes</label>
-              <input type="file" name="files"/>
-            </div>
-
-            <button type="submit">Cadastrar arrecadação</button>
-          </form>
+          </main>
         </div>
-      </main>
-    </div>
+      </div>
     </div>
   );
 }
