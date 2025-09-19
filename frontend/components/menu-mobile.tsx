24d721f7a43863567f8f162612910f9ec9a5fbe3
Analice Coimbra <analicecoimbra2000@gmail.com>
2025-09-19 19:19:00 -0300

    Fix: restricted-areas buttons

--- frontend/components/menu-mobile.tsx
+++ frontend/components/menu-mobile.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 
-export default function MenuMobile() {
+interface Properties {
+  idTime: number
+}
+
+export default function MenuMobile({idTime}: Properties) {
   return (
     <nav className="mobile-nav">
-      <Link href="/(restricted)/[userId]/new-contribution"><button className="donation-menu-button">Cadastrar novas contribuições</button></Link>
-      <Link href="/team-history"><button className="donation-menu-button">Histórico de contribuições</button></Link>
+      <Link href={`/(restricted)/${idTime}/new-contribution`}><button className="donation-menu-button">Cadastrar novas contribuições</button></Link>
+      <Link href={`/(restricted)/${idTime}/team-history`}><button className="donation-menu-button">Histórico de contribuições</button></Link>
     </nav>
   );
 }
