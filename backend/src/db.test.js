6a3f2763fcbf8d38dd70d635ee3f7cae202b2e91
mariah alice <loboalicepereira@gmail.com>
2025-09-27 23:32:30 -0300

    db.test

--- backend/src/db.test.js
+++ backend/src/db.test.js
@@ -1,24 +1,13 @@
-import mysql from "mysql2/promise";
-import "dotenv/config";
+import mongoose from "mongoose";
 
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB } = process.env;
-
-const tryConnection = async () => {
-  try {
-    const connection = await mysql.createConnection({
-      host: MYSQL_HOST,
-      user: MYSQL_USER,
-      database: MYSQL_DB,
-      password: MYSQL_PASSWORD
-    })
-    const [rows] = await connection.query("SELECT NOW() AS agora");
-    console.log("Conectando ao MySQL - Data/Hora:", rows[0].agora);
-    await connection.end();
-  } catch (err) {
-    console.error("There was an error: " + err.message);
+const url = "";
+async function run(){
+  try{
+    await mongoose.connect(url);
+    console.log("conectadoooo!!!!");
+  }catch(err){
+    console.error("nãooooooo tá conectado...........", err.message);
+    process.exit(1);
   }
-};
-
-export default tryConnection;
-
-tryConnection();
+}
+run();
