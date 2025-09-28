18beb6940aa9f5ac71e278f00bf16066f652ad98
Analice Coimbra <analicecoimbra2000@gmail.com>
2025-09-28 17:14:32 -0300

    feat/history-page template
    template base da pagina de histórico

--- frontend/app/(restricted)/[userId]/team-history/page.tsx
+++ frontend/app/(restricted)/[userId]/team-history/page.tsx
@@ -5,6 +5,8 @@ import { useParams } from "next/navigation";
 import MenuMobile from "@/components/menu-mobile";
 import MenuDesktop from "@/components/menu-desktop";
 import { fetchData } from "@/hooks/fetch-user-profile";
+import renderContribution from "@/components/render-contribution";
+import RenderContribution from "@/components/render-contribution";
 
 export default function TeamHistory() {
   const params = useParams();
@@ -15,18 +17,18 @@ export default function TeamHistory() {
   const [user, setUser] = React.useState<any>(null);
   const [team, setTeam] = React.useState<any>(null);
 
-  useEffect(() => {
-    const fetchTeamData = async () => {
-      const data = await fetchData(userId);
-      setUser(data?.user);
-      setTeam(data?.team);
-    };
-    fetchTeamData();
-  }, [userId]);
+  // useEffect(() => {
+  //   const fetchTeamData = async () => {
+  //     const data = await fetchData(userId);
+  //     setUser(data?.user);
+  //     setTeam(data?.team);
+  //   };
+  //   fetchTeamData();
+  // }, [userId]);
 
   return (
     <div className="w-screen h-screen overflow-x-clip">
-      <div className="absolute left-0 top-0">
+      <div className="flex flex-col left-0 top-0">
         <header>
           <button
             type="button"
@@ -38,13 +40,16 @@ export default function TeamHistory() {
             {" "}
             ☰{" "}
           </button>
+           <h1 className="text-2xl font-semibold text-green-800 self-center mt-4">
+              Histórico de contribuições
+            </h1>
         </header>
       </div>
 
       <div
-        className={`${
-          menuOpen ? "ml-[270px]" : ""
-        } w-full h-full flex justify-center items-center transition-all duration-300 ease-in-out`}
+        className={`w-full h-full flex justify-center pt-4 transition-all duration-300 ease-in-out ${
+          menuOpen ? "md:ml-[270px]" : "ml-0"
+        }`}
       >
         {/* Menu lateral quando está no desktop/tablet */}
         <MenuDesktop
@@ -55,13 +60,24 @@ export default function TeamHistory() {
 
         {/* Menu rodapé quando está no mobile */}
         <MenuMobile raUsuario={team?.RaUsuario || 10000000} />
+
+        {/* main page do historico - todas as contribuições do grupo */}
+        <main className="w-screen max-w-[1300px] p-1.5 md:mt-0">
+          {/*grid grid-cols-1 md:grid-cols-3*/}
+          <div className="flex flex-col gap-2 mx-3">
+            <h3 className="text-2xl uppercase font-semibold text-primary">
+              {team?.NomeTime ? team?.NomeTime : "Nome do time aparecerá aqui"}
+            </h3>
+            <h4 className="mb-3 text-xl text-primary">
+              Turma {team?.Turma ? team?.Turma : "X"} | Yº Edição
+            </h4>
+          </div>
+          <div className="bg-[#f5f5f5] mx-4 grid grid-cols-1 md:grid-cols-3 gap-4.5 ">
+            <RenderContribution/>
+          </div>
+
+        </main>
       </div>
-
-      <main className="w-screen max-w-[1300px] mt-30 md:mt-0 grid grid-cols-1 md:grid-cols-3">
-
-
-
-      </main>
     </div>
   );
 }
---
+++ frontend/components/render-contribution.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+export default function RenderContribution() {
+
+    return (
+        <>
+         gerar o html da contribuição
+
+
+
+         
+        </>
+    );
+}
