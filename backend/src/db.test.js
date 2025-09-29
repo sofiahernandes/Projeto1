10dd81b65cefefdd3273d440711fddd1db3d512c
mariah alice <loboalicepereira@gmail.com>
2025-09-28 21:21:07 -0300

    upds finais do db

--- backend/src/db.test.js
+++ backend/src/db.test.js
@@ -1,13 +1,28 @@
-import mongoose from "mongoose";
+import { PrismaClient } from "@prisma/client";
 
-const url = "";
-async function run(){
-  try{
-    await mongoose.connect(url);
-    console.log("conectadoooo!!!!");
-  }catch(err){
-    console.error("nãooooooo tá conectado...........", err.message);
-    process.exit(1);
+const prisma = new PrismaClient();
+
+async function tryConnection() {
+  try {
+    
+    const newUser = await prisma.usuario.create({
+      data: {  RaUsuario: 25027757,
+      NomeUsuario:"Analice Coimbra",
+      EmailUsuario:"analice.carneiro@edu.fecap",
+      SenhaUsuario:"123@projeto01",
+      TelefoneUsuario:"1145367282",
+      Turma:"1MA",},
+    });
+
+    console.log("Usuário criado:", newUser);
+
+    const users = await prisma.usuario.findMany();
+    console.log("Usuários no banco:", users);
+  } catch (err) {
+    console.error("Erro de conexão:", err.message);
+  } finally {
+    await prisma.$disconnect();
   }
 }
-run();
+
+tryConnection();
