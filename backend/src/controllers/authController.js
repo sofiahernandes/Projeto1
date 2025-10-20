de214303506806beffd74f4aa429f71d241f941b
mariah alice <loboalicepereira@gmail.com>
2025-10-20 10:00:28 -0300

    update: controllers & schema.prisma

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,20 @@
+// //import db from '../db.js'
+// import bcrypt from 'bcrypt'
+// import {createdToken, denyToken} from '../services/tokenServices'
+
+// const sanitizeUser = (u) => ({  RaUsuario: u.rausuario,
+//           SenhaUsuario: u.senhausuario,})
+
+// export const register = async (req, res) => {
+//     const {rausuario, senha } = req.body 
+//     if (!rausuario || !senha ) {
+//     return res.status(400).json({ error: "Coloque o seu Ra e sua Senha"})
+//     } 
+
+//     try{
+//         const [exists] = await prisma.query('')
+
+//     } catch{
+
+//     }
+// }
