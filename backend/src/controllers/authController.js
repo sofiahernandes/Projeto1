cc6335817a0021673870422ca7c37033e7e8eb41
mariah alice <loboalicepereira@gmail.com>
2025-10-20 16:25:21 -0300

    update: form-login
    + add login mentor and admin mentor
    + add tokenServices and config

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,20 +1,19 @@
-// //import db from '../db.js'
-// import bcrypt from 'bcrypt'
-// import {createdToken, denyToken} from '../services/tokenServices'
+//import db from '../db.js'
+import bcrypt from "bcrypt";
+import { createdToken, denyToken } from "../services/tokenServices";
 
-// const sanitizeUser = (u) => ({  RaUsuario: u.rausuario,
-//           SenhaUsuario: u.senhausuario,})
+const sanitizeUser = (u) => ({
+  RaUsuario: u.rausuario,
+  SenhaUsuario: u.senhausuario,
+});
 
-// export const register = async (req, res) => {
-//     const {rausuario, senha } = req.body 
-//     if (!rausuario || !senha ) {
-//     return res.status(400).json({ error: "Coloque o seu Ra e sua Senha"})
-//     } 
+export const register = async (req, res) => {
+  const { rausuario, senha } = req.body;
+  if (!rausuario || !senha) {
+    return res.status(400).json({ error: "Coloque o seu Ra e sua Senha" });
+  }
 
-//     try{
-//         const [exists] = await prisma.query('')
-
-//     } catch{
-
-//     }
-// }
+  try {
+    const [exists] = await prisma.query("");
+  } catch {}
+};
--- backend/src/services/tokenServices.js
+++ backend/src/services/tokenServices.js
@@ -1,29 +1,28 @@
 import jwt from "jsonwebtoken";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
-const denylist = new Set()
+const denylist = new Set();
 
 export const createToken = (payload, options = {}) => {
-    const jti = uuidv4 // id único de token 
-    const token = jwt.sign(
-        {...payload, jti}, 
-        process.env.JWT_SECRET,
-        {expiresIn: process.env.JWT_EXPIRES || '2h', ... options }
-    )
-    return {token, jti}
- }
+  const jti = uuidv4; // id único de token
+  const token = jwt.sign({ ...payload, jti }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES || "2h",
+    ...options,
+  });
+  return { token, jti };
+};
 
-export const isDenied = (jti) => denylist.has(jti) 
+export const isDenied = (jti) => denylist.has(jti);
 
 export const denyToken = (jti) => {
-    if (jti) denylist.add(jti)
-}
+  if (jti) denylist.add(jti);
+};
 
-export const verifyToken = (token) => new Promise ((resolve, reject) => {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded)=> {
-        if (err) return reject(err)
-        if (isDenied(decoded.jti)) return reject(new Error ('Token denylisted'))
-        return resolve(decoded)
-    })
-})
-
+export const verifyToken = (token) =>
+  new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) return reject(err);
+      if (isDenied(decoded.jti)) return reject(new Error("Token denylisted"));
+      return resolve(decoded);
+    });
+  });
