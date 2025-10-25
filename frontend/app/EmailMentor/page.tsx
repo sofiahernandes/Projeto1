c886d16fba4b3c13560ef9cc5ef356876ba79316
Analice Coimbra <analicecoimbra2000@gmail.com>
2025-10-25 15:05:45 -0300

    fix: mentor history page name

--- frontend/app/EmailMentor/page.tsx
+++ frontend/app/EmailMentor/page.tsx
@@ -3,9 +3,14 @@
 import React, { SetStateAction, useEffect } from "react";
 import BackHome from "@/components/back-home";
 import { useParams } from "next/navigation";
-import RenderContribution from "@/components/render-contribution";
+import RenderContribution from "@/components/grid-contribution";
 import { fetchData } from "@/hooks/fetch-user-profile";
 import RecordsModal from "@/components/records-modal";
+import { makeContributionColumns } from "@/components/contribution-table/columns";
+import { DataTable } from "@/components/contribution-table/data-table";
+
+
+{/*fazer a tabela aqui ainda, e colocar a pagina do mentor dentro do restricted, fazer cadastro dele e aparecer as contribuitosn */}
 
 export default function MentorVision() {
   const params = useParams();
@@ -17,14 +22,14 @@ export default function MentorVision() {
   const [selectedContribution, setSelectedContribution] =
     React.useState<any>(null);
 
-    useEffect(() => {
-      const fetchTeamData = async () => {
-        const data = await fetchData(userId);
-        setUser(data?.user);
-        setTeam(data?.team);
-      };
-      fetchTeamData();
-    }, [userId]);
+    // useEffect(() => {
+    //   const fetchTeamData = async () => {
+    //     const data = await fetchData(userId);
+    //     setUser(data?.user);
+    //     setTeam(data?.team);
+    //   };
+    //   fetchTeamData();
+    // }, [userId]);
 
   return (
     <div className="min-h-dvh w-full overflow-y-hidden overflow-x-hidden flex flex-col bg-[#f4f3f1]/60">
@@ -56,6 +61,7 @@ export default function MentorVision() {
             Turma {user?.Turma ? user?.Turma : "X"} | Yº Edição
           </h4>
         </div>
+         {/*colocar aqui o if botao selecionado, mostrar yabela inves de card */}
         <RenderContribution
           onSelect={(contribution: any) => {
             setSelectedContribution(contribution);
