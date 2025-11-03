88d9910e24dd12aa6c50faf1a957446e242fd45a
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2025-11-03 10:15:45 -0300

    feat: Hero image

--- frontend/components/hero.tsx
+++ frontend/components/hero.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 
 import Link from "next/link";
+import heroBackground from "@/assets/texture.png";
 
 const Hero = () => {
   return (
@@ -19,6 +20,8 @@ const Hero = () => {
         </div>
         <Link href="#public-graph"><p className="text-3xl z-300 text-white pb-10 animate-bounce">↓</p></Link>
       </div>
+
+      <div className="absolute w-full h-full top-0 right-0 opacity-55"><Image src={heroBackground} alt="Background Hero" fill /></div>
     </section>
   );
 };
